70cf241cfa4bae448ca390ad06cf98a7b50f10e4
Daniel Senften <daniel@senften.org>
2018-12-11 15:16:33 +0100

    Weitere Klassen für den ersten Testfall (UserTest).

---
+++ src/test/java/llqa/TestApplicationPersistence.java
@@ -0,0 +1,25 @@
+package llqa;
+
+import llqa.repository.BaseRepositoryFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@Configuration
+@EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
+public class TestApplicationPersistence {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplicationPersistence.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
